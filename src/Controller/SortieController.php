3a7bef5d14f6b2c2d9245352136626afb3925cdb
egreil <esteban.greil@orange.fr>
2024-06-24 16:24:40 +0200

    Ajout du controller des sorties avec l'affichage des sorties, la création, la suppresion, l'update et l'affichage du détail des sorties

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+#[Route('/sortie', name: 'sortie_')]
+class SortieController extends AbstractController
+{
+    #[Route('/update/{id}', name:'update',requirements: ['id'=> '\d+'])]
+    #[Route('/create', name:'create')]
+    public function createSortie(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        int $id=null,
+        SortieRepository $sortieRepository
+    ): Response
+    {
+
+        if($id){
+            $sortie=$sortieRepository->find($id);
+        }
+        else{
+            $sortie=new Sortie();
+        }
+        $sortieForm=$this->createForm(SortieType::class,$sortie);
+        $sortieForm->handleRequest($request);
+
+        if($sortieForm->isSubmitted() && $sortieForm->isValid()){
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+            return $this->redirectToRoute('sortie_details',[
+                'id'=>$sortie->getId()
+            ]);
+        }
+
+        return $this->render('sortie/create.html.twig', [
+            'sortie' => $sortie,
+            'form' => $sortieForm,
+        ]);
+    }
+    #[Route('/list',name:'list')]
+    #[Route('/',name:'')]
+    public function listSorties(
+        SortieRepository $sortieRepository,
+        Request $request,
+    ){
+        $sorties=$sortieRepository->findSorties($request);
+
+        return $this->render('sortie/list.html.twig',[
+            'sorties'=>$sorties
+        ]
+
+        );
+    }
+
+
+    #[Route('/delete/{id}', name:'delete',requirements: ['id'=> '\d+'])]
+    public function deleteSortie(
+        EntityManagerInterface $entityManager,
+        int $id=null,
+        SortieRepository $sortieRepository
+    ): Response
+    {
+        if($id){
+            $sortie=$sortieRepository->find($id);
+        }
+        $entityManager->remove($sortie);
+        $entityManager->flush();
+        return $this->render('sortie/list.html.twig', [
+        ]);
+    }
+    #[Route('/details/{id}', name:'details',requirements: ['id'=> '\d+'])]
+    public function detailSortie(
+    SortieRepository $sortieRepository,
+    int $id=null,
+    ){
+        if($id){
+            $sortie=$sortieRepository->find($id);
+        }
+        return $this->render('sortie/details.html.twig',[
+            'sortie'=>$sortie
+        ]);
+    }
+}
---
+++ src/Form/SortieType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\SubmitButton;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('idSortie')
+            ->add('nom')
+            ->add('dateHeureDebut', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('duree')
+            ->add('dateLimiteInscription', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('nbInscriptionMax')
+            ->add('infosSortie')
+            ->add('etat')
+            ->add('button', SubmitType::class,[
+                'label' => 'Ajouter la sortie'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+            'required'=>false
+        ]);
+    }
+}
