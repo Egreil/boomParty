fdbcdcc5b4a22d63ade34c0d352248c83e33f5e7
mbouhbou2023 <meriem49bouhbou@gmail.com>
2024-06-24 16:25:02 +0200

    entities : ville, lieu et sortie

---
+++ src/LieuService/LieuService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\LieuService;
+
+use App\Entity\Lieu;
+use Doctrine\ORM\EntityManagerInterface;
+
+class LieuService
+
+
+{
+    private EntityManagerInterface $entityManager;
+public function __construct(EntityManagerInterface $entityManager)
+{
+    $this->entityManager = $entityManager;
+}
+
+public function createLieu(string $nom, string $rue, float $lat, float $lng) : Lieu
+{
+    $lieu = new Lieu();
+    $lieu->setNom($nom);
+    $lieu->setRue($rue);
+    $lieu->setLatitude($lat);
+    $lieu->setLongitude($lng);
+
+    $this->entityManager->persist($lieu);
+    $this->entityManager->flush();
+
+    return $lieu;
+}
+
+}
