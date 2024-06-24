2ece2f5e4227e5862e9acc51815beb6b20d60444
mbouhbou2023 <meriem49bouhbou@gmail.com>
2024-06-24 15:24:36 +0200

    entities : ville, lieu et sortie

---
+++ src/Controller/VilleController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ville;
+use App\Repository\VilleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/villes', name: 'villes_')]
+class VilleController extends AbstractController
+{
+    #[Route('/list', name: 'list')]
+    public function list(
+        VilleRepository $repo,
+        Request $request
+    ): Response {
+        $codePostal = $request->query->get('codePostal');
+
+        if (!$codePostal) {
+            throw $this->createNotFoundException('Code postal manquant !');
+        }
+
+        $villes = $repo->findBy(['codePostal' => $codePostal]);
+
+        return $this->render('villes/list.html.twig', [
+            'villes' => $villes,
+        ]);
+    }
+}
